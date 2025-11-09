238874d22e8e460c531043e77ab6f5c4a4fdb641
javi <framirez76@uabc.edu.mx>
2025-11-08 23:20:48 -0800

    Add unit conversion dropdown with styling for improved user experience

--- script.js
+++ script.js
@@ -3,3 +3,15 @@
 document.querySelector(".search").addEventListener("focus", (event) => {
 	event.currentTarget.querySelector("input").focus();
 });
+
+const dropdown = document.querySelector("#units-dropdown");
+
+document.querySelector("#units").addEventListener("click", (event) => {
+	if (dropdown.classList.contains("dropdown-visible")) {
+		dropdown.classList.remove("dropdown-visible");
+		dropdown.classList.add("dropdown-invisible");
+	} else {
+		dropdown.classList.remove("dropdown-invisible");
+		dropdown.classList.add("dropdown-visible");
+	}
+});

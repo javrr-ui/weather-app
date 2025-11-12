4a0c9b806c14cda3d47db88c56a18a3a696fe3b1
javi <framirez76@uabc.edu.mx>
2025-11-11 19:44:34 -0800

    Hide dropdown when user clicks outside of it

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+const dropdown = document.querySelector("#units-dropdown");
+const unitsButton = document.querySelector("#units");
+
+//dropdown
+document.addEventListener("click", (event) => {
+	if (
+		!dropdown.contains(event.target) &&
+		!unitsButton.contains(event.target)
+	) {
+		if (dropdown.classList.contains("dropdown-visible")) {
+			dropdown.classList.remove("dropdown-visible");
+			dropdown.classList.add("dropdown-invisible");
+		}
+	}
+});
+
 // Seach bar
 
 document.querySelector(".search").addEventListener("focus", (event) => {
 	event.currentTarget.querySelector("input").focus();
 });
-
-const dropdown = document.querySelector("#units-dropdown");
 
 document.querySelector("#units").addEventListener("click", (event) => {
 	if (dropdown.classList.contains("dropdown-visible")) {
